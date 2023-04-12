55cd32e642e37a7d648867b0cdd7187e920fa251
JinHwan <jhk1411@gmail.com>
2023-04-13 06:12:44 +0900

    api key update

--- InfoSourceCredit.py
+++ InfoSourceCredit.py
@@ -1,10 +1,26 @@
-﻿import mysql.connector
-import openai
-import os
+﻿import openai
+import mysql.connector
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
 
-# Connect to the database
+def generate_response(prompt):
+    completions = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    message = completions.choices[0].text.strip()
+    return message
+
+def classify_site(site_name, evaluation_criteria):
+    prompt = f"Classify {site_name} according to {evaluation_criteria}"
+    response = generate_response(prompt)
+    return response
+
 db = mysql.connector.connect(
     host="localhost",
     user="username",
@@ -12,21 +28,14 @@ db = mysql.connector.connect(
     database="database_name"
 )
 
-# Fetch the site list and evaluation criteria from the database
 cursor = db.cursor()
-cursor.execute("SELECT site_name, evaluation_criteria FROM site_table")
-site_list = cursor.fetchall()
+cursor.execute("SELECT id, site_name, evaluation_criteria FROM site_table")
+site_data = cursor.fetchall()
 
-# Define the ChatGPT API function
-def classify_site(site_name, evaluation_criteria):
-    prompt = f"Classify {site_name} according to {evaluation_criteria}"
-    response = generate_response(prompt)
-    return response
-
-# Classify the sites and store the results in a new table
-for site in site_list:
-    site_name = site[0]
-    evaluation_criteria = site[1]
+for site in site_data:
+    site_id = site[0]
+    site_name = site[1]
+    evaluation_criteria = site[2]
     classification = classify_site(site_name, evaluation_criteria)
-    cursor.execute("INSERT INTO classification_table (site_name, evaluation_criteria, classification) VALUES (%s, %s, %s)", (site_name, evaluation_criteria, classification))
+    cursor.execute("UPDATE site_table SET classification=%s WHERE id=%s", (classification, site_id))
     db.commit()
