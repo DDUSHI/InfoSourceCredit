d9154398ff2978c8ba02d19cbc215aa52957a18f
JinHwan <jhk1411@gmail.com>
2023-04-18 03:12:02 +0900

    DB update require

--- InfoSourceCredit.py
+++ InfoSourceCredit.py
@@ -37,15 +37,16 @@ for website in websites:
     # Escape special characters in the website address
     try:
         response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=f"Assess the reliability of {address} based on the following criteria: authority, accuracy, objectivity, currency, coverage, consistency.",
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.5
-        )
+        engine="text-davinci-002",
+        prompt=f"Assess the reliability of {address} based on the following criteria: authority, accuracy, objectivity, currency, coverage, consistency. Score each criterion from 1 (lowest) to 5 (highest). Your Answering format should be 'score of authority,score of accuracy,score of objectivity,score of currency,score of coverage,score of consistency'",
+        max_tokens=64,  # Use a smaller max_tokens value to simplify the response
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
+
         updated_scores = response.choices[0].text.split(',')
-        print(updated_scores)
+        updated_scores[0] = updated_scores[0].replace("\n\n", "")
         # Update the corresponding reliability criteria in the site_table table
         if len(updated_scores) >= 6:
             sql = "UPDATE site_table SET authority=%s, accuracy=%s, objectivity=%s, currency=%s, coverage=%s, consistency=%s, score_change_date=%s WHERE site_address = %s"
@@ -57,7 +58,7 @@ for website in websites:
             for i in range(len(current_scores)):
                 if current_scores[i] != updated_scores[i]:
                     issue_item = ["authority", "accuracy", "objectivity", "currency", "coverage", "consistency"][i]
-                    old_score = current_scores[i].strip()
+                    old_score = current_scores[i]
                     new_score = updated_scores[i].strip()
                     reason = "Credit score change"
                     date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
